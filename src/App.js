c9beb94ed640f9ee149416ceb527f754910e9001
Tommaso <cenedesetommaso@gmail.com>
2024-05-29 12:18:40 +0200

    pagina ricerca

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/navbar';
+import ComboBox from './components/combobox';
+import FreeSolo from './components/serchInput';
+
+import ActionAreaCard from './components/card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ResponsiveAppBar/>
       </header>
+      <body>
+        <div className='container'>
+          <div className='filter-container'>
+            <h4>Filtra per:</h4>
+            <ComboBox label='Categoria'/>
+            <ComboBox label='Provincia'/>
+            <FreeSolo />
+          </div>
+          <div className='grid-container'>
+              <ActionAreaCard></ActionAreaCard>
+              <ActionAreaCard></ActionAreaCard>
+              <ActionAreaCard></ActionAreaCard>
+              <ActionAreaCard></ActionAreaCard>
+              <ActionAreaCard></ActionAreaCard>
+              <ActionAreaCard></ActionAreaCard>
+              <ActionAreaCard></ActionAreaCard>
+              <ActionAreaCard></ActionAreaCard>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { CardActionArea } from '@mui/material';
+import '../medico-competente.png'
+
+export default function ActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="220"
+          image="https://st2.depositphotos.com/1743476/5738/i/450/depositphotos_57385697-stock-photo-confident-mature-doctor.jpg"
+          alt="immagine di copertina"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Mario Rossi
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            <strong>Provincia:</strong> Treviso <br/>
+            <strong>Professione:</strong> Osteopata <br/>
+            Lorem ipsum dolor sit amet. Sed sunt expedita et nihil sint et quos aperiam est alias dicta qui fuga vitae? Ut optio distinctio rem eius ullam et tempora quasi. Aut delectus tempora 33 deleniti rerum eos totam ipsam quo velit pariatur vel quibusdam molestiae qui tempore suscipit.
+          </Typography>
+          <Button variant="contained" sx={{width: '100%', marginTop: '10px'}}>Prenota visita</Button>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
